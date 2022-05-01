ccc97e990f54cbfe9cd90b0a63c60efe9e606d82
akita <541731587@qq.com>
2022-05-01 09:24:14 +0800

    Initial commit

---
+++ isprs/potsdam.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+
+class Potsdam:
+    def __init__(self, dataset_path, target_path):
+        self.my_re = re.compile(r'\d+_\d+')
+        self.dataset_path = dataset_path
+        self.target_path = target_path
+        self.DSM_path = os.path.join(dataset_path, 'DSM')
+        self.RGB_path = os.path.join(dataset_path, 'RGB')
+        self.Label_path = os.path.join(dataset_path, 'Label')
+        self.file_flag = [self.my_re.findall(name)[-1] for name in os.listdir(self.DSM_path)]
+
+    def start_dealWith(self):
+        print(self.file_flag)
+
+
+if __name__ == '__main__':
+    po = Potsdam(dataset_path=r'../Potsdam', target_path=r'../Dataset')
+    po.start_dealWith()
