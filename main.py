fc5d657d2676d293a328ff16c52f4afa74f942cf
akita <541731587@qq.com>
2022-10-24 15:53:47 +0800

    展示

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from isprs.vaihingen import Vaihingen
 from isprs.potsdam import Potsdam
 
 if __name__ == '__main__':
-    v = Vaihingen(dataset_path=r'C:\Users\akitalee\Desktop\Vaihingen',
-                  target_path=r'C:\Users\akitalee\Desktop\VaihingenDataset\train')
-    v.start_dealWith(split_size=256)
+    v = Vaihingen(dataset_path=r'数据集源路径',
+                  target_path=r'目标路径')
+    v.start_dealWith(split_size='分割的尺寸大小')
