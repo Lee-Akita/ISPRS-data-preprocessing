1e469ccf5b9f5980ed7c285549ac281f6c5b5420
akita <541731587@qq.com>
2022-10-24 15:56:56 +0800

    测试

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
 import os
-import re
-import cv2
 import numpy as np
 from PIL import Image
 import matplotlib.pylab as plt
 
-dataset_path = r'C:\Users\akitalee\Desktop\Vaihingen'
+dataset_path = r'分割后的数据集'
 
 # 测试接口
 if __name__ == '__main__':
-    myre = re.compile('\d+')
-    rgb_path = os.listdir(os.path.join(dataset_path, 'RGB'))
-    dsm_path = os.listdir(os.path.join(dataset_path, 'DSM'))
-    label_path = os.listdir(os.path.join(dataset_path, 'Label'))
-    print(label_path)
-    print(dsm_path)
-    print(rgb_path)
+    a = np.array(Image.open(os.path.join(dataset_path, 'RGB', '20.png')))
+    b = np.array(Image.open(os.path.join(dataset_path, 'Label', '20.png')))
+    plt.subplot(1, 2, 1)
+    plt.imshow(a)
+    plt.subplot(1, 2, 2)
+    plt.imshow(b)
+    plt.show()
