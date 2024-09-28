a08a65d32048ab04cd29ad02e620bbd00a1962e6
Lee-Akita <541731587@qq.com>
2024-09-28 23:27:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from isprs.vaihingen import Vaihingen
 from isprs.potsdam import Potsdam
 
+
+# 开始
+
 if __name__ == '__main__':
     v = Vaihingen(dataset_path=r'数据集源路径',
                   target_path=r'目标路径')
