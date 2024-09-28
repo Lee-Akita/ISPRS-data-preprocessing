efe03c12b44d7faefabbe770e32094d021ef0c6f
Lee-Akita <541731587@qq.com>
2024-09-28 23:38:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from isprs.potsdam import Potsdam
 # 开始
 
 if __name__ == '__main__':
+    print('hello')
     v = Vaihingen(dataset_path=r'数据集源路径',
                   target_path=r'目标路径')
     v.start_dealWith(split_size='分割的尺寸大小')
